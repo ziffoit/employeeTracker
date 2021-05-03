802c933d6f52b0a9e16adcd24d050a6a7fe7ebc5
Kevin Hanson <ziffoit@gmail.com>
2021-05-03 11:07:15 -0600

    employee table

--- schema.sql
+++ schema.sql
@@ -14,3 +14,13 @@ salary DECIMAL NOT NULL,
 department_id INT NOT NULL,
 CONSTRAINT fk_department FOREIGN KEY(department_id) REFERENCES department(id) ON DELETE CASCADE
 );
+
+REATE TABLE employee(
+id INT AUTO_INCREMENT PRIMARY KEY,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INT,
+CONSTRAINT fk_role FOREIGN KEY(role_id) REFERENCES role(id) ON DELETE SET NULL,
+manager_id INT,
+CONSTRAINT fk_manager FOREIGN KEY(manager_id) REFERENCES employee(id) ON DELETE SET NULL   
+);
