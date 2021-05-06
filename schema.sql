c973ed8908d1b384f750023c0ded10e373decf06
Kevin Hanson <ziffoit@gmail.com>
2021-05-05 20:31:50 -0600

    package.json

--- schema.sql
+++ schema.sql
@@ -15,7 +15,7 @@ department_id INT NOT NULL,
 CONSTRAINT fk_department FOREIGN KEY(department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
-REATE TABLE employee(
+CREATE TABLE employee(
 id INT AUTO_INCREMENT PRIMARY KEY,
 first_name VARCHAR(30) NOT NULL,
 last_name VARCHAR(30) NOT NULL,
